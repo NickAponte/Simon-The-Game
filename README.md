8ea2456f231c1982e7bc85e286048dc7a30b1720
NickAponte <apontenick@aol.com>
2021-02-21 15:38:41 -0500

    Refactoring code heavily

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Simon-The-Game
+
 A Recreation of Simon, the popular memory game!
 
-
-
-
 # Notes
+
 Step 1, get a simple html button representation, and pattern going
 
-The pattern should probably be represented by an array that is randomly generated. The array should consist of numbers from 1-4 corresponding with the Simon color buttons. 
+The pattern should probably be represented by an array that is randomly generated. The array should consist of numbers from 1-4 corresponding with the Simon color buttons.
 
-Step 2- check the pattern 
+Step 2- check the pattern
 
-Then, when the player attempts to re-create the pattern, the player will populate their own array with the buttons they clicked.  While being populated, the function should check to make sure each added index === the array of the computer generated pattern. If not, the player lost. 
+Then, when the player attempts to re-create the pattern, the player will populate their own array with the buttons they clicked. While being populated, the function should check to make sure each added index === the array of the computer generated pattern. If not, the player lost.
---
+++ script2.js
@@ -0,0 +1,52 @@
+// a computer array and a player array
+// step 1, computer generates 1 random number, push to computer array
+// step 2, allow clicking,
+// step 3 push users clicked # onto user array
+//step compare the current indexes ()
+// if matches, next step, else game over
+// css set pointer events to none, add the remove click class to the boxes
+const cells = document.querySelectorAll('.cell');
+const playButton = document.getElementById('play');
+const cell1 = document.querySelector('#one');
+const cell2 = document.querySelector('#two');
+const cell3 = document.querySelector('#three');
+const cell4 = document.querySelector('#four');
+playButton.addEventListener('click', gameStart)
+let computerSequence = [1,2,3];
+let playerSequence = [];
+let turn = 0
+
+
+function turnCounter(){
+turn += 1;
+
+
+
+}
+
+
+function gameStart() {
+	console.log("gamestart")
+	playButton.classList.add('removeClick');
+
+}
+
+
+
+cells.forEach((cell) => {
+	cell.addEventListener('click', (event) => {
+		event.preventDefault;
+		console.log(cell.getAttribute('data-number'));
+		playerSequence.push(cell.getAttribute('data-number'));
+		console.log(playerSequence);
+	});
+});
+
+
+function removeClick() {
+	cell1.classList.add('removeClick');
+	cell2.classList.add('removeClick');
+	cell3.classList.add('removeClick');
+	cell4.classList.add('removeClick');
+}
+// console.log(playButton);
